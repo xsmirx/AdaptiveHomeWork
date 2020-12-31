ae76ee6f45e96974498005f83d0feb08c2688299
xsmirx <xsmirx@gmai.com>
2021-01-01 00:10:52 +0300

    js fixed

--- js/scrypt.js
+++ js/scrypt.js
@@ -1,7 +1,17 @@
-$(document).ready(function () {
-    $('.header__burger').click(function(event){
-        $('.header__burger').toggleClass('header__burger_active'),
-        $('.header__menu').toggleClass('header__menu_active'),
-        $('body').toggleClass('lock')
-    })
-})
+// $(document).ready(function () {
+//     $('.header__burger').click(function(event){
+//         $('.header__burger').toggleClass('header__burger_active'),
+//         $('.header__menu').toggleClass('header__menu_active'),
+//         $('body').toggleClass('lock')
+//     })
+// })
+
+let burger = document.querySelector('.header__burger');
+let menu = document.querySelector('.header__menu');
+let body = document.querySelector('body')
+
+burger.onclick = function() {
+    burger.classList.toggle('header__burger_active');
+    menu.classList.toggle('header__menu_active');
+    body.classList.toggle('lock');
+  };
